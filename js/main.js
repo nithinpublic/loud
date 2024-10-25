b3426249c879900d1b2f9c6b334b453ab2f1e938
nithinpublic <nithinpublic@gmail.com>
2024-10-25 04:22:41 +0100

    m

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+function playMusic(){
+    var music = new Audio('/audio/flush_sound.mp3');
+    music.play();
+    }
+
+
+
 document.querySelector("#flush-button").addEventListener("click", ()=>{
     document.querySelector("textarea").value = ""
+    playMusic()
+    
 })
