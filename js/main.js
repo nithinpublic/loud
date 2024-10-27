4a38c6cdf1b31a7d10e2cc60f45762c29b38fb37
nithinpublic <nithinpublic@gmail.com>
2024-10-27 06:50:31 +0000

    m

--- js/main.js
+++ js/main.js
@@ -24,14 +24,14 @@ function flushOld() {
     delay = 1
     waterElements = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890@#%^&œþ¥øßðƒ©"
 
-    thoughts = textbox.value
+    userInput = textbox.value
 
 
     airgapLength = 200
     function animateFlush() {
         airgap = ""
         water = ""
-        
+
         for (let i = 0; i < airgapLength; i++) {
             airgap += "\u00a0"
         }
@@ -40,7 +40,7 @@ function flushOld() {
         }
 
         if (c < 10) {
-            textbox.value = thoughts + airgap + water
+            textbox.value = userInput + airgap + water
             c += 1
             airgapLength -= 1
             setTimeout(animateFlush, delay);
@@ -50,47 +50,70 @@ function flushOld() {
 }
 
 
-String.prototype.replaceAt = function(index, replacement) {
+String.prototype.replaceAt = function (index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
-function flush() {
-    thoughts = textbox.value
-    waterElements = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890@#%&œþ¥øßðƒ©♥♥♥●₳"
+function flushOld2() {
+    userInput = textbox.value
+    waterElements = "qwertyuiopa♥♥♥sdfghjk♥♥♥lzxcvbnmQWE♥♥♥RTYUIOPASDFGHJKLZXCVBNM1234567890@#%&œþ¥øßðƒ©♥♥♥●₳"
 
-    wateredText = thoughts
+    wateredText = userInput
 
     i = 0
     acceleration = 0
-    function thoughtReplacer(){
-        
-        
-        if (i < wateredText.length){
-            for(j=0;j<=i;j++){
-                randomCharacter = waterElements.charAt(Math.ceil((waterElements.length) * (Math.random())))
-                wateredText = wateredText.replaceAt(j,randomCharacter)
+    depth = 500
+
+    function thoughtReplacer() {
+        if (i < wateredText.length) {
+            for (j = 0; j <= i; j++) {
+                randomCharacter = waterElements.charAt(Math.floor((waterElements.length) * (Math.random())))
+                wateredText = wateredText.replaceAt(j, randomCharacter)
             }
-            
-            i= i + 1 + acceleration
+            i = i + 1 + acceleration
             acceleration = acceleration + 0.1
             textbox.value = wateredText
-            setTimeout(thoughtReplacer, (300/(i+1))+10)
-
+            setTimeout(thoughtReplacer, (300 / (i + 1)) + 10)
         } else {
             console.log("done");
-            if (i < 500) {
-                randomCharacter = waterElements.charAt(Math.ceil((waterElements.length) * (Math.random())))
+            if (i < depth) {
+                randomCharacter = waterElements.charAt(Math.floor((waterElements.length) * (Math.random())))
                 wateredText += randomCharacter
                 thoughtReplacer()
-
             }
-            
         }
     }
     thoughtReplacer()
+}
 
 
+function flush() {
+    userInput = textbox.value
+    waterElements = "qwertyuiopa♥♥♥sdfghjk♥♥♥lzxcvbnmQWE♥♥♥RTYUIOPASDFGHJKLZXCVBNM1234567890@#%&œþ¥øßðƒ©♥♥♥●₳"
+    waterElements = "░▒▓█"
 
-  
+    wateredText = userInput
+
+    i = 0
+    acceleration = 0
+    depth = 3000
     
-   
+    function thoughtReplacer() {
+        if (i < depth) {
+            for (j = 0; j <= i; j++) {
+                randomCharacter = waterElements.charAt(Math.floor((waterElements.length) * (Math.random())))
+                
+                wateredText = wateredText.replaceAt(j, randomCharacter)
+            }
+            if(i >= userInput.length) {
+                wateredText += randomCharacter
+            } 
+            i = i + 1 + acceleration
+            acceleration = acceleration + 0.1
+            textbox.value = wateredText
+            setTimeout(thoughtReplacer, (300 / (i + 1)) + 10)
+        } else {
+            console.log("done");
+        }
+    }
+    thoughtReplacer()
 }
