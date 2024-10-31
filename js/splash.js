32523b7f230eeabf72797a6a29ce0b54a0fd97f8
nithinpublic <nithinpublic@gmail.com>
2024-10-31 22:32:50 +0000

    m

--- js/splash.js
+++ js/splash.js
@@ -13,24 +13,42 @@ console.log("dfsdf");
 console.log(getRandomColorFromSet());
 
 noiseCharSet = "███████████████████▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░LOUD"
-screenDepth = 250
+screenDepth = 200
+
+dissipate = false
+
+setTimeout(() => {
+    dissipate = true
+}, 300);
+
+setTimeout(() => {
+    splashScreen.style.display = "none"
+}, 700);
 
 function fillSplashScreen() {
     splashScreen.innerHTML = ""
     for (i = 0; i < screenDepth; i++) {
         randomChar = noiseCharSet.charAt(Math.floor((noiseCharSet.length) * (Math.random())))
         splashScreen.innerHTML += String("<span style='color:" + getRandomColorFromSet() + ";'>" + randomChar + "</span>")
+        
     }
     if (splashScreen.style.display != "none") {
         setTimeout(() => {
+            if (dissipate) {
+                screenDepth = Math.floor(screenDepth/2)
+                console.log(screenDepth);
+                
+                //splashScreen.style.fontSize = String(screenDepth+"px")
+                // splashScreen.style.top = "24px"
+                // splashScreen.style.translate = "-50% 0px"
+            }
             fillSplashScreen()
         }, 40);
     }
+    
 }
 fillSplashScreen()
 
-setTimeout(() => {
-    splashScreen.style.display = "none"
-}, 700);
+
 
 
